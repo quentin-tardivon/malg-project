8cddb0c28ad7ec77cd583da262f89107f55a01ec
Mcdostone <yann.prono@telecomnancy.net>
2017-05-06 18:46:15 +0200

    Push document, frama-c for last algo

--- frama-c/bubble_sort.c
+++ frama-c/bubble_sort.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
+#include <limits.h>
 
+
+/*@ ensures \result == 0;
+*/
 int main() {
   int array[100], n, c, d, swap;
 
   printf("Enter number of elements\n");
   scanf("%d", &n);
+  //@ assert INT_MIN <= n <= INT_MAX;
 
   printf("Enter %d integers\n", n);
 
+  /*@ loop invariant INT_MIN <= c <= INT_MAX;
+      loop variant n - c;
+   */
   for (c = 0; c < n; c++) {
     scanf("%d", &array[c]);
   }
 
   for (c = 0; c < (n-1); c++) {
+    //@ assert c < n - 1;
     for (d = 0; d < n-c-1; d++) {
+      //@ assert d < n - c - 1;
       if (array[d] > array[d+1]) {
         swap = array[d];
+        //@ assert swap == array[d];
         array[d] = array[d+1];
+        //@ assert array[d] == array[d + 1];
         array[d+1] = swap;
+        //@ assert array[d + 1] == swap;
+        //@ assert array[d] < array[d + 1];
       }
     }
+    //@ assert d == n - c - 1;
   }
+  //@ assert c == n - 1;
+
 
   printf("Sorted list in ascending order:\n");
 
--- frama-c/last_algorithm.c
+++ frama-c/last_algorithm.c
@@ -1,31 +1,56 @@
 #include <stdio.h>
 
+
+/*@ assigns \nothing;
+  behavior x_eq_zero:
+      assumes x == 0;
+      ensures \result == 1;
+    behavior x_eq_one:
+      assumes x == 1;
+      ensures \result == 1;
+    behavior x_eq_other:
+      assumes x != 0 && x != 1;
+      ensures 1 == 1;
+*/
 int p1(int x) {
   int a,b,c,i,r,fin;
   a = 1;
+  //@ assert a == 1;
   b = 1;
+  //@ assert b == 1;
   i = 2;
+  //@ assert i == 2;
 
   if(x == 0) {
+    //@ assert x == 0;
     r = 1;
+    //@ assert r == 1;
   }
   else {
+    //@ assert x != 0;
     if (x ==1 ) {
       r = 1;
+      //@ assert r == 1 && x == 1;
     }
     else {
       while (i-1 < x) {
         i = i+1;
         c = a;
+        //@ assert c == a;
         a = b;
+        //@ assert b == a;
         b = 2*c+2*b;
       }
     }
     r = b;
+    //@ assert r == b;
   }
   return (r);
 }
 
+/*@
+  ensures \result == 0;
+  */
 int main() {
   int v;
   printf("Entrer la valeur pour v\n");
