d87c6f941fe9d34ad50ac071d3873c6d76e75ff3
Mcdostone <yann.prono@telecomnancy.net>
2017-05-06 17:15:56 +0200

    I don't understand frama-c, bye <3

--- Makefile
+++ Makefile
@@ -1,9 +1,13 @@
 FRAMACDIR = frama-c
 SRCDIR = src
 TLADIR = tla
-FRAMAC_PGM = frama-c-gui
-#FRAMAC_OPTS = -wp -val
-FRAMAC_OPTS = -wp
+FRAMAC_PGM_GUI = frama-c-gui
+FRAMAC_PGM = frama-c
+#FRAMAC_OPTS = -wp  -wp-print -wp-rte -va X.c -val
+FRAMAC_OPTS = -wp -report -wp-split
+
+frama-gui:
+	$(FRAMAC_PGM_GUI) $(FRAMAC_OPTS) $(file) -print
 
 frama:
-	$(FRAMAC_PGM) $(FRAMAC_OPTS) $(file) -print &
+		$(FRAMAC_PGM) $(FRAMAC_OPTS) $(file) -print
--- frama-c/floyd_triangle.c
+++ frama-c/floyd_triangle.c
@@ -1,48 +1,35 @@
 #include <stdio.h>
 #include <limits.h>
 
-/*
- - On check si n est bien un integer valide
- - on verifie que aucune variable exterieur, ni arguments soit modifiés par la méthode (revient à faire
- ensures n == \old(n))
- - Si n negatif ou nul
-        - le résultat est 1
- - Si n positif
-        - le résultat est (((n*(n+1)/2)+1)
+/*@ ensures \result == 0;
+    assigns \nothing;
 */
-
-/* @ requires n > INT_MIN;
- * @ requires n >= 0;
- * @ requires n < INT_MAX;
- * @ assigns \nothing
- * @ requires n >= 0;
- * @ behavior n_negative_nul:
-        assumes n <= 0;
-        ensures \result == 1;
- * @ behavior n_pos:
-        assumes n > 0;
-        ensures \result == (((n*(n+1)/2)+1);
-*/
-int pink_floyd(int n) {
-  int i, c, a = 1;
+int main() {
+  int n, i, c, a = 1;
+  printf("Enter the number of rows of Floyd's triangle to print\n");
+  scanf("%d", &n);
+  //@ assert INT_MIN <= n <= INT_MAX;
   //@ assert a == 1;
 
+  /*@ loop invariant c <= n + 1;
+      loop assigns i;
+      loop variant n - i;
+   */
   for (i = 1; i <= n; i++) {
-    //@ assert i <= n;
+    //@ assert i <= n + 1;
+    /*@ loop invariant c <= i + 1;
+        loop assigns c, a;
+        loop variant i - c;
+     */
     for (c = 1; c <= i; c++) {
       //@ assert c <= i;
       printf("%d ",a);
       a++;
     }
+    //@ assert c > i;
     printf("\n");
   }
-  return a;
-}
-
-int main() {
-  int n;
-  printf("Enter the number of rows of Floyd's triangle to print\n");
-  scanf("%d", &n);
-  pink_floyd(n);
+  //@ assert i > n;
+  //@ assert a == ((n*(n + 1) / 2) + 1);
   return 0;
 }
